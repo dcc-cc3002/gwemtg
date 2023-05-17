0a067e8ea25ae2fd7de97c2687810c194df5fe70
hugodiazroa <hugodiazroa@github.com>
2023-05-16 22:31:56 -0400

    nuevos tests con elementos minimos

--- src/test/scala/gwent/BoardTest/BoardTest.scala
+++ src/test/scala/gwent/BoardTest/BoardTest.scala
@@ -1,11 +1,29 @@
 package cl.uchile.dcc
 package gwent.BoardTest
 
+import gwent.Board.*
+import gwent.Side.*
 import gwent.Player.*
 import gwent.Carta.*
 import munit.*
 
 
 class BoardTest extends munit.FunSuite {
+  var cero: Carta = new Carta("Carta 0")
+  var uno: Carta = new Carta("Carta 1")
+  var dos: Carta = new Carta("Carta 2")
+  var tres: Carta = new Carta("Carta 3")
+  var jugador: Player = new Player("Hugo", List(uno, dos))
+  var npc: ComputerPlayer = new ComputerPlayer("Marvin", List(cero, uno))
+
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    cero = new Carta("Carta 0")
+    uno = new Carta("Carta 1")
+    dos = new Carta("Carta 2")
+    tres = new Carta("Carta 3")
+    jugador = new Player("Hugo", List(uno, dos))
+    npc = new ComputerPlayer("Marvin", List(cero, uno))
+  }
 
 }
