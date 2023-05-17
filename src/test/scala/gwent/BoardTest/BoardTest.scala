1f1be623c3e6e40631e0446e79ffce975bb7ab50
hugodiazroa <hugodiazroa@github.com>
2023-05-16 22:22:17 -0400

    se anxade SideTest y BoardTest a test

---
+++ src/test/scala/gwent/BoardTest/BoardTest.scala
@@ -0,0 +1,11 @@
+package cl.uchile.dcc
+package gwent.BoardTest
+
+import gwent.Player.*
+import gwent.Carta.*
+import munit.*
+
+
+class BoardTest extends munit.FunSuite {
+
+}
---
+++ src/test/scala/gwent/SideTest/SideTest.scala
@@ -0,0 +1,12 @@
+package cl.uchile.dcc
+package gwent.SideTest
+
+
+import gwent.Player.*
+import gwent.Carta.*
+import munit.*
+
+
+class SideTest  extends munit.FunSuite {
+
+}
