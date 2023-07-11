d8c6b04956a9cbfda3ce98457bd0c7b3f53c7e10
hugodiazroa <hugodiazroa@gmail.com>
2023-07-10 20:38:07 -0400

    basic tests for player

--- src/test/scala/gwent/PlayerTest.scala
+++ src/test/scala/gwent/PlayerTest.scala
@@ -1,8 +1,53 @@
 package cl.uchile.dcc
 package gwent
 
+import gwent.cards.*
 import munit.*
 
 class PlayerTest extends munit.FunSuite{
+  var jugador: Player = _
+  var troop1: CloseCombatCard = _
+  var troop2: CloseCombatCard = _
+  var troop3: CloseCombatCard = _
 
+  var ranged1: RangedCombatCard = _
+  var ranged2: RangedCombatCard = _
+  var ranged3: RangedCombatCard = _
+
+  var catapult1: SiegeCombatCard = _
+  var catapult2: SiegeCombatCard = _
+  var catapult3: SiegeCombatCard = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    troop1 = new CloseCombatCard("troop1", "pedro", 1)
+    troop2 = new CloseCombatCard("troop2", "pablo", 2)
+    troop3 = new CloseCombatCard("troop3", "jose", 3)
+
+    ranged1 = new RangedCombatCard("ranged1", "eliana", 1)
+    ranged2 = new RangedCombatCard("ranged2", "maria", 2)
+    ranged3 = new RangedCombatCard("ranged3", "alia", 3)
+
+    catapult1 = new SiegeCombatCard("catapult1", "isis", 1)
+    catapult2 = new SiegeCombatCard("catapult2", "dracula", 2)
+    catapult3 = new SiegeCombatCard("catapult3", "atenea", 3)
+
+    val jugador = new Player("Hugo", 2, List(troop1, troop3, ranged1, ranged3, catapult1, catapult3), List(troop2, ranged2, catapult2))
+
+  }
+
+
+  test("test para equals") {
+    val p1: Player = new Player("Hugo", 2, List(troop1, troop3, ranged1, ranged3, catapult1, catapult3), List(troop2, ranged2, catapult2))
+    val p2: Player = new Player("Lain", 2, List(), List())
+    p1.equals(jugador)
+    jugador.equals(p1)
+    assert(!(p1.equals(p2)))
+  }
+
+  test("un jugador tiene bien su hashCode y debe poder ser comparado con canEqual") {
+    val p1: Player = new Player("Hugo", 2, List(troop1, troop3, ranged1, ranged3, catapult1, catapult3), List(troop2, ranged2, catapult2))
+    assertEquals(p1.##, jugador.##)
+    assert(p1.canEqual(jugador))
+    assert(jugador.canEqual(p1))
+  }
 }
