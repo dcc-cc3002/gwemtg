f665b1eedb7366c8c5ad8213a7d0fa0bde1ba93c
hugodiazroa <hugodiazroa@gmail.com>
2023-07-16 22:25:11 -0400

    fix local error in intellij, regarding game

--- src/main/scala/gwent/Game.scala
+++ src/main/scala/gwent/Game.scala
@@ -1,6 +1,7 @@
 package cl.uchile.dcc
 package gwent
 
+import gwent.Player
 import gwent.board.*
 import gwent.cards.*
 
@@ -9,8 +10,8 @@ import gwent.cards.*
 /**
  * A class representing a game of Gwent.
  * @param board The board of the game.
- * @param player1 The first player of the game.
- * @param player2 The second player of the game.
+ * @param _player1 The first player of the game.
+ * @param _player2 The second player of the game.
  *
  * here the game is played, the players play their cards and the board is updated
  * here the design pattern observer is used, the board is the observable and the players are the observers
