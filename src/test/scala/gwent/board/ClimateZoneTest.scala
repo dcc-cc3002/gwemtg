a10cff4dd5c7dbf72e87a8e266424672fca6d73e
Lya Diaz <hugodiazroa@gmail.com>
2023-08-07 17:36:12 +0000

    try and fail: update tests for mana

--- src/test/scala/gwent/board/ClimateZoneTest.scala
+++ src/test/scala/gwent/board/ClimateZoneTest.scala
@@ -5,8 +5,7 @@ import gwent.cards.*
 import gwent.Player
 import munit.*
 class ClimateZoneTest extends munit.FunSuite{
-  /** tablero vacio */
-  var tablero1: Board = new Board()
+  var tablero1: Board = _
   var tablero2: Board = _
   var tablero3: Board = _
 
@@ -18,28 +17,51 @@ class ClimateZoneTest extends munit.FunSuite{
   var zona2range: RangeZone = _
   var zona2siege: MarginalZone = _
 
-  var troop1: CloseCombatCard = new CloseCombatCard("troop1", "pedro", 1)
-  var troop2: CloseCombatCard = new CloseCombatCard("troop2", "pablo", 2)
-  var troop3: CloseCombatCard = new CloseCombatCard("troop3", "jose", 3)
+  var troop1: CloseCombatCard = _
+  var troop2: CloseCombatCard = _
+  var troop3: CloseCombatCard = _
 
-  var ranged1: RangedCombatCard = new RangedCombatCard("ranged1", "eliana", 1)
-  var ranged2: RangedCombatCard = new RangedCombatCard("ranged2", "maria", 2)
-  var ranged3: RangedCombatCard = new RangedCombatCard("ranged3", "alia", 3)
+  var ranged1: RangedCombatCard = _
+  var ranged2: RangedCombatCard = _
+  var ranged3: RangedCombatCard = _
 
-  var catapult1: SiegeCombatCard = new SiegeCombatCard("catapult1", "isis", 1)
-  var catapult2: SiegeCombatCard = new SiegeCombatCard("catapult2", "dracula", 2)
-  var catapult3: SiegeCombatCard = new SiegeCombatCard("catapult3", "atenea", 3)
-
-
-  var jugador: Player = _
+  var catapult1: SiegeCombatCard = _
+  var catapult2: SiegeCombatCard = _
+  var catapult3: SiegeCombatCard = _
 
   var climate1: WeatherCard = _
+  var climate2: WeatherCard = _
+
+  var jugador: Player = _
+  var jugador2: Player = _
+
+  var partida: Game = _
+
 
   override def beforeEach(context: BeforeEach): Unit = {
+    troop1 = new CloseCombatCard("troop1", "pedro", 1, 1)
+    troop2 = new CloseCombatCard("troop2", "pablo", 2, 2)
+    troop3 = new CloseCombatCard("troop3", "jose", 3, 3)
+
+    ranged1 = new RangedCombatCard("ranged1", "eliana", 1, 1)
+    ranged2 = new RangedCombatCard("ranged2", "maria", 2, 2)
+    ranged3 = new RangedCombatCard("ranged3", "alia", 3, 3)
+
+    catapult1 = new SiegeCombatCard("catapult1", "isis", 1, 1)
+    catapult2 = new SiegeCombatCard("catapult2", "dracula", 2, 2)
+    catapult3 = new SiegeCombatCard("catapult3", "atenea", 3, 3)
+
     climate1 = new WeatherCard("climate1", "lluvia")
+    climate2 = new WeatherCard("climate2", "nieve")
+
+    jugador = new Player("Hugo", 2, List(troop1, troop3, ranged1, ranged3, catapult1, catapult3), List(troop2, ranged2, catapult2, climate2))
+    jugador2 = new Player("Lain", 2, List(), List())
+
+    partida = new Game(tablero1, jugador, jugador)
 
     tablero1 = new Board()
     tablero2 = new Board(new MarginalZone(List(catapult1, catapult2, catapult3)), new RangeZone(List(ranged1, ranged2, ranged3)), new MeleeZone(List(troop1, troop2, troop3)), new ClimateZone(List(climate1)), new MeleeZone(List(troop1, troop2, troop3)), new RangeZone(List(ranged1, ranged2, ranged3)), new MarginalZone(List(catapult1, catapult2, catapult3)))
+
     zona1siege = new MarginalZone(List(catapult1, catapult2, catapult3))
     zona1range = new RangeZone(List(ranged1, ranged2, ranged3))
     zona1melee = new MeleeZone(List(troop1, troop2, troop3))
@@ -47,21 +69,8 @@ class ClimateZoneTest extends munit.FunSuite{
     zona2melee = new MeleeZone(List(troop1, troop2, troop3))
     zona2range = new RangeZone(List(ranged1, ranged2, ranged3))
     zona2siege = new MarginalZone(List(catapult1, catapult2, catapult3))
+
     tablero3 = new Board(zona1siege, zona1range, zona1melee, zonaclimate, zona2melee, zona2range, zona2siege)
-
-    troop1 = new CloseCombatCard("troop1", "pedro", 1)
-    troop2 = new CloseCombatCard("troop2", "pablo", 2)
-    troop3 = new CloseCombatCard("troop3", "jose", 3)
-
-    ranged1 = new RangedCombatCard("ranged1", "eliana", 1)
-    ranged2 = new RangedCombatCard("ranged2", "maria", 2)
-    ranged3 = new RangedCombatCard("ranged3", "alia", 3)
-
-    catapult1 = new SiegeCombatCard("catapult1", "isis", 1)
-    catapult2 = new SiegeCombatCard("catapult2", "dracula", 2)
-    catapult3 = new SiegeCombatCard("catapult3", "atenea", 3)
-
-    jugador = new Player("Hugo", 2, List(troop1, troop3, ranged1, ranged3, catapult1, catapult3), List(troop2, ranged2, catapult2))
   }
 
   test("constructor"){
